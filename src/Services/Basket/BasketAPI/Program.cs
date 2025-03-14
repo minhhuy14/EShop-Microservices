8c97730ec70c2de134aeaa474092dec8515cc61c
Lê Minh Huy <minhhuy14012003@gmail.com>
2025-03-14 16:23:48 +0700

    Add distributed caching using Redis

--- src/Services/Basket/BasketAPI/Program.cs
+++ src/Services/Basket/BasketAPI/Program.cs
@@ -41,6 +41,21 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options=>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis")!;
+});
+
+// builder.Services.AddScoped<IBasketRepository>(provider => 
+// {
+//         var basketRepository = provider.GetRequiredService<IBasketRepository>();
+//
+//         return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+// });
+
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
