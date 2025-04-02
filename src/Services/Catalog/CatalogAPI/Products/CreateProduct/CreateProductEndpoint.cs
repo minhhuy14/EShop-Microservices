62bd928da490274ef0b59df6c85b27f9fe316729
Lê Minh Huy <minhhuy14012003@gmail.com>
2025-04-02 09:57:47 +0700

    Integrate EF Core with SQLite and implement CRUD operations for Coupons

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -2,7 +2,6 @@ namespace CatalogAPI.Products.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
 
-public record CreateProductResponse(Guid Id);
 
 public class CreateProductEndpoint:ICarterModule
 {
