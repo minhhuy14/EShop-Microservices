4ee632be0b1842b5619644195d4cd898cf10c4f7
Lê Minh Huy <minhhuy14012003@gmail.com>
2025-04-03 09:46:05 +0700

    Consuming Discount Grpc service from Basket service

--- src/Services/Basket/BasketAPI/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/BasketAPI/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,17 +1,34 @@
+using Discount.Grpc;
+
 namespace BasketAPI.Basket.StoreBasket;
 
-public class StoreBasketCommandHandler(IBasketRepository repository):ICommandHandler<StoreBasketCommand,StoreBasketResponse>
+public class StoreBasketCommandHandler(IBasketRepository repository,DiscountProtoService.DiscountProtoServiceClient discountProto):ICommandHandler<StoreBasketCommand,StoreBasketResponse>
 {
     public async Task<StoreBasketResponse> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        ShoppingCart cart=command.Cart;
+        //TODO: communicate with Discount.Grpc and calculate latest prices of product into shopping cart
         
-        await repository.StoreBasket(cart,cancellationToken);
+        await DeductDiscount(command.Cart,cancellationToken);
+        
+        await repository.StoreBasket(command.Cart,cancellationToken);
 
         return new StoreBasketResponse
         {
-            UserName=cart.UserName
+            UserName=command.Cart.UserName
         };
-
     }
+    
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest
+            {
+                ProductName = item.ProductName
+            }, cancellationToken:cancellationToken);
+            
+            item.Price -= coupon.Amount;
+        }
+    }
+    
 }
